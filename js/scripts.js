fe85947efd03045cbe07d354f89d41fc09b4cca5
Bobby Martin and Isaac Shoman <bobby@>
2018-04-04 10:56:24 -0700

    Add spec 7, set player object

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,13 @@ var turnScore = 0;
 var rollArray = [];
 var playerScore = [];
 
+function Player(name) {
+  this.playerName = name;
+  this.playerScore = [];
+  this.turnScore = 0;
+  this.rollArray = [];
+}
+
 function diceRoll() {
   var roll = Math.floor(Math.random() * 6 + 1);
   if (roll === 1) {
@@ -14,9 +21,13 @@ function diceRoll() {
 
 function turnTotal() {
   // Hold button function
-  for (i=0; i < rollArray.length; i++) {
-    turnScore += (rollArray[i]);
+  if (playerScore[playerScore.length - 1] >= 100) {
+    return "You Win!"
+  } else {
+    for (i=0; i < rollArray.length; i++) {
+      turnScore += (rollArray[i]);
+    }
+    playerScore.push(turnScore);
+    console.log(playerScore);
   }
-  playerScore.push(turnScore)
-  console.log(playerScore);
 };
