c677ee7351aa7887e97de5741f2d644594c7a5a5
Bobby Martin and Isaac Shoman <isaac@>
2018-04-04 14:51:20 -0700

    Add full functionality for single player

--- js/scripts.js
+++ js/scripts.js
@@ -12,40 +12,28 @@ Player.prototype.diceRoll = function () {
   var roll = Math.floor(Math.random() * 6 + 1);
   var counter = 0;
   if (roll === 1) {
-    console.log("YOU ROLLED A 1");
     this.rollArray = [];
     this.turnScore = 0;
     counter = 0;
   } else {
     this.rollArray.push(roll);
-    if (this.totalPlayerScore >= 100) {
-      return "You Win!"
-    } else {
-      // this.rollArray.map(function(num) {
-      //   counter += num
-      // });
-      for (i = 0; i < this.rollArray.length; i++) {
-        counter += this.rollArray[i]
-      }
-      console.log(this.rollArray, "ROLL ARRAY");
-      this.turnScore = counter;
+    for (i = 0; i < this.rollArray.length; i++) {
+      counter += this.rollArray[i]
     }
+    console.log(this.rollArray, "ROLL ARRAY");
+    this.turnScore = counter;
   }
 };
 
 Player.prototype.hold = function() {
   // Hold button function
   var counter = 0;
-  if (this.totalPlayerScore >= 100) {
-    alert("You Win!");
-  } else {
-    this.playerScore.push(this.turnScore);
+  this.playerScore.push(this.turnScore);
 
-    this.playerScore.map(function(num) {
-      counter += num;
-    });
-    this.totalPlayerScore = counter;
-  }
+  this.playerScore.map(function(num) {
+    counter += num;
+  });
+  this.totalPlayerScore = counter;
   console.log(this.playerScore, "THIS IS THE PLAYERSCORE");
 };
 
@@ -63,14 +51,18 @@ $(function() {
     // Roll Click
     $("#roll").click(function(){
       playerOne.diceRoll();
-      console.log(playerOne.turnScore, "CURRENT TURN SCORE");
+      if (playerOne.rollArray.length === 0) {
+        $("#dice h1").text("You rolled a 1. Your turn is over.");
+      } else {
+        $("#dice h1").text(playerOne.rollArray[playerOne.rollArray.length - 1]);
+        console.log(playerOne.turnScore, "CURRENT TURN SCORE");
+      }
     });
-
 
     $("#hold").click(function() {
       playerOne.hold();
       if(playerOne.totalPlayerScore >= 100) {
-        $("#win").text(playerOne.playerName);
+        $("#win").text(' ' + playerOne.playerName);
         $(".win").show();
       }
       $("#playerScore").text(playerOne.totalPlayerScore);
