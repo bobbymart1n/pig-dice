53f9f8a10e588ef915370e53c0a4e025e04dd596
bobby martin and isaac shoman <bobby@>
2018-04-05 16:46:38 -0700

    Add style and win condition

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 // Player Object
+var active = true;
 function Player(name) {
   this.playerName = name;
   this.playerScore = [];
   this.turnScore = 0;
   this.totalPlayerScore = 0;
   this.rollArray = [];
-  this.active = true;
 }
 
 // Player Prototypes
@@ -33,7 +33,6 @@ Player.prototype.hold = function() {
     counter += num;
   });
   this.totalPlayerScore = counter;
-  this.active = false;
 };
 
 Player.prototype.turnReset = function () {
@@ -55,7 +54,6 @@ Player.prototype.computer = function () {
   this.computerHold();
 };
 
-
 // User Interface Logic
 $(function() {
   $("#twoPlayer").click(function() {
@@ -70,40 +68,68 @@ $(function() {
     var playerTwoName = $("#playerTwoName").val();
     $(this).hide();
     $("#gameBoard").show();
-    $("h1#userName").text(playerName);
+    $("h1#userName span").text(playerName);
     if (typeof playerTwoName === "string"){ // Runs Two Player Logic on
       var playerOne = new Player(playerName);
       var playerTwo = new Player(playerTwoName);
       $("#playerButtons").show();
       $("div.two-player-scores").show();
-
-      $("#roll").click(function(){
-        debugger;
-        if (playerOne.active === true && playerTwo.active === true) {
-          console.log(playerOne);
-          console.log(playerTwo);
-          playerOne.diceRoll();
-          if(playerOne.rollArray.length === 0) {
-            $("#dice h1").text("You rolled a 1. Your turn is over.");
-            this.active = false;
+      if(playerOne.totalPlayerScore <= 100 && playerTwo.totalPlayerScore <= 100) {
+        $("#roll").click(function(){
+          if (active === true) {
+            playerOne.diceRoll();
+            if(playerOne.rollArray.length === 0) {
+              // debugger;
+              $("h1#userName span").text(playerTwo.playerName);
+              playerOne.turnReset();
+              active = false;
+              $("#dice div").removeClass();
+            } else {
+              $("#player-turnscore").text(playerOne.turnScore);
+              $("#dice div").removeClass();
+              $("#dice div").addClass('dice-face' + playerOne.rollArray[playerOne.rollArray.length - 1].toString());
+            }
           } else {
-            $("#player-turnscore").text(playerOne.turnScore);
-            $("#dice h1").text(playerOne.rollArray[playerOne.rollArray.length - 1]);
+            $("h1#userName span").text(playerTwo.playerName);
+            playerTwo.diceRoll();
+            if(playerTwo.rollArray.length === 0) {
+              playerTwo.turnReset();
+              active = true;
+              $("#dice div").removeClass();
+              $("h1#userName span").text(playerOne.playerName);
+            } else {
+              $("#player-turnscore").text(playerTwo.turnScore);
+              $("#dice div").removeClass();
+              $("#dice div").addClass('dice-face' + playerTwo.rollArray[playerTwo.rollArray.length - 1].toString());
+            }
           }
-        } else if (playerTwo.active === true && playerOne.active === false) {
-          console.log(playerOne);
-          console.log(playerTwo);
-          playerTwo.diceRoll();
-          if(playerTwo.rollArray.length === 0) {
-            $("#dice h1").text("You rolled a 1. Your turn is over.");
-            this.active = false;
-            playerOne.active = true;
+        });
+        $("#hold").click(function(){
+          if (active === true) {
+            playerOne.hold();
+            $("h1#userName span").text(playerTwo.playerName);
+            $("#playerOneScore").text(playerOne.totalPlayerScore);
+            playerOne.turnReset();
+            $("#dice div").removeClass();
+            active = false;
           } else {
-            $("#player-turnscore").text(playerTwo.turnScore);
-            $("#dice h1").text(playerTwo.rollArray[playerTwo.rollArray.length - 1]);
+            playerTwo.hold();
+            $("h1#userName span").text(playerOne.playerName);
+            $("#playerTwoScore").text(playerTwo.totalPlayerScore);
+            playerTwo.turnReset();
+            $("#dice div").removeClass();
+            active = true;
           }
+        });
+      } else {
+        if(playerOne.totalPlayerScore >= 100) {
+          $(".win").show();
+          $("#win").text(playerOne.playerName);
+        } else if (playerTwo.totalPlayerScore >= 100) {
+          $(".win").show();
+          $("#win").text(playerTwo.playerName);
         }
-      });
+      }
     }
   });
 });
